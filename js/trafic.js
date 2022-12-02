13caa27d6ee1ec812ede3b511609faf8f0467839
Muas09 <116275467+Muas09@users.noreply.github.com>
2022-12-02 11:35:57 +0700

    done

---
+++ js/trafic.js
@@ -0,0 +1,14 @@
+function getGreencolor() {
+  document.getElementById("myImg").src="./img/green.png";
+}
+function getYellowcolor() {
+  document.getElementById("myImg").src="./img/yellow.png";
+}
+
+
+function getRedcolor() {
+  document.getElementById("myImg").src="./img/red.png";
+}
+
+
+
